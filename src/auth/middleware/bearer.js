9c65b7fb488bbc9e26d045cf3bb329245081174c
Kellen-Linse <kblinse06@gmail.com>
2022-01-18 20:47:00 -0800

    remove commits

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -10,16 +10,12 @@ const bearerAuth = async (req, res, next) => {
     const token = req.headers.authorization.split(' ').pop();
 
     const validUser = await users.authenticateWithToken(token);
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3, validUser);
-
     if(validUser) {
     req.user = validUser;
     req.token = validUser.token;
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", 1);
     next();
     }
   } catch (e) {
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", 3);
     res.status(403).send('Invalid Login');
   }
 }
